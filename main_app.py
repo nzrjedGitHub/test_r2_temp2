2cf5bb0c0cdb6d664361278da09921aa71c7b7f1
nzrjedGitHub <98401179+nzrjedGitHub@users.noreply.github.com>
2024-06-13 19:17:23 +0300

    finished

--- main_app.py
+++ main_app.py
@@ -43,11 +43,94 @@ class InstrScr(Screen):
     def next(self):
         self.manager.current = "pulse1"
 
+class PulseScr(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        instr = Label(text=txt_test1)
+
+        line = BoxLayout(size_hint=(0.8, None), height="30sp")
+        lbl_result = Label(text="Введіть результат:", halign="right")
+        self.in_result = TextInput(text="0", multiline=False)
+
+        line.add_widget(lbl_result)
+        line.add_widget(self.in_result)
+        self.btn = Button(
+            text="Продовжити", size_hint=(0.3, 0.2), pos_hint={"center_x": 0.5}
+        )
+        self.btn.on_press = self.next
+        outer = BoxLayout(orientation="vertical", padding=8, spacing=8)
+        outer.add_widget(instr)
+        outer.add_widget(line)
+        outer.add_widget(self.btn)
+        self.add_widget(outer)
+    def next(self):
+        self.manager.current = "sits"
+
+
+class CheckSits(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        instr = Label(text=txt_sits)
+        self.btn = Button(
+            text="Продовжити", size_hint=(0.3, 0.2), pos_hint={"center_x": 0.5}
+        )
+        self.btn.on_press = self.next
+        outer = BoxLayout(orientation="vertical", padding=8, spacing=8)
+        outer.add_widget(instr)
+        outer.add_widget(self.btn)
+        self.add_widget(outer)
+
+    def next(self):
+        self.manager.current = "pulse2"
+
+
+class PulseScr2(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        instr = Label(text=txt_test3)
+        line1 = BoxLayout(size_hint=(0.8, None), height="30sp")
+        lbl_result1 = Label(text="Результат:", halign="right")
+        self.in_result1 = TextInput(text="0", multiline=False)
+        line1.add_widget(lbl_result1)
+        line1.add_widget(self.in_result1)
+        line2 = BoxLayout(size_hint=(0.8, None), height="30sp")
+        lbl_result2 = Label(text="Результат після відпочинку:", halign="right")
+        self.in_result2 = TextInput(text="0", multiline=False)
+        line2.add_widget(lbl_result2)
+        line2.add_widget(self.in_result2)
+        self.btn = Button(
+            text="Завершити", size_hint=(0.3, 0.2), pos_hint={"center_x": 0.5}
+        )
+        self.btn.on_press = self.next
+        outer = BoxLayout(orientation="vertical", padding=8, spacing=8)
+        outer.add_widget(instr)
+        outer.add_widget(line1)
+        outer.add_widget(line2)
+        outer.add_widget(self.btn)
+        self.add_widget(outer)
+
+    def next(self):
+        self.manager.current = "result"
+
+
+class Result(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.outer = BoxLayout(orientation="vertical", padding=8, spacing=8)
+        self.instr = Label(text="place for results")
+        self.outer.add_widget(self.instr)
+        self.add_widget(self.outer)
+
 
 class HeartCheck(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(InstrScr(name="instr"))
+        sm.add_widget(PulseScr(name="pulse1"))
+        sm.add_widget(CheckSits(name="sits"))
+        sm.add_widget(PulseScr2(name="pulse2"))
+        sm.add_widget(Result(name="result"))
         return sm
     
 app = HeartCheck()
